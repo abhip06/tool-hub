8f44fc83de51202146877cb926899dc570d10763
Abhinav Patel <abhipatel1806@gmail.com>
2024-09-03 12:43:34 +0530

    Changed Footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,14 @@
 
 const Footer = () => {
     return (
-        <div className="flex flex-wrap flex-col bg-gray-900 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-28 py-16 text-white justify-center bottom-0">
+        <div className="flex flex-wrap flex-col bg-gray-300 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-28 py-16 text-gray-800 justify-center bottom-0">
             <div className="flex gap-2 flex-col mb-10">
                 <div className="text-4xl font-bold">
                     <span>Tools</span><span className="text-violet-500">Hub</span>
                 </div>
                <p className="text-sm">Tools for your daily use at one place.</p>
             </div>
-            <hr className="bg-white my-5" />
+            <div className="bg-gray-500 h-[1px] my-5"></div>
             <span className='text-base text-center'>&copy; 2024 - Tools Hub - All rights reserved.</span>
         </div>
     )
